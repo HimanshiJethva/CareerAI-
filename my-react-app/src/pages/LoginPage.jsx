c10c2372fdf38b9e48f1c48dcd3274eb1de30cef
BhatiaMuskan9512 <muskanmbhatiya9512@gmail.com>
2026-04-04 15:18:11 +0530

    saving my work before pull

--- my-react-app/src/pages/LoginPage.jsx
+++ my-react-app/src/pages/LoginPage.jsx
@@ -63,26 +63,6 @@ function LoginPage({ setView }) {
         return Object.keys(newErrors).length === 0
       }
 
-  //   return (
-  //     <div className="auth-container">
-  //       <div className="auth-card">
-  //         <button className="back-btn" onClick={() => setView('landing')}>← Back</button>
-  //         {errors.api && <p style={{color:"red"}}>{errors.api}</p>}
-  //         <h2 style={{fontFamily: 'Playfair Display', fontSize: '2.5rem', marginBottom: '1rem'}}>Welcome Back</h2>
-  //         <input type="email" value={email} onChange={(e)=>setEmail(e.target.value)} placeholder="Email Address" className="auth-input" />
-  //         {errors.email && <p style={{color:"red"}}>{errors.email}</p>}
-  //         <input type="password" value={password} onChange={(e)=>setPassword(e.target.value)} placeholder="Password" className="auth-input" />
-  //         {errors.password && <p style={{color:"red"}}>{errors.password}</p>}
-  //         <button onClick={handleLogin} className="btn-primary" style={{width: '100%'}} disabled={loading}>
-  //           {loading ? "Logging you in..." : "Login"}</button>
-  //         <p style={{marginTop: '1.5rem'}}>
-  //           Don't have an account? 
-  //           <span className="auth-link" onClick={() => setView('signup')}> Sign Up</span>
-  //         </p>
-  //       </div>
-  //     </div>
-  //   );
-  // }
    return (
       <div className="auth-container">
         <div className="auth-card">
